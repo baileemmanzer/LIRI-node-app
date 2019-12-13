b9c90a26131ef215b8ea55f98b953dbe391ec4ea
Bailee Michaela <baileemanzer3@gmail.com>
2019-12-13 11:38:50 -0700

    setting variables, commands

--- liri.js
+++ liri.js
@@ -1,1 +1,37 @@
 require("dotenv").config();
+
+//Code required to import the keys.js file
+var keys = require("./keys.js");
+var axios = require("axios");
+var fs = require("fs");
+var moment = require("moment");
+
+var spotify = new Spotify(keys.spotify);
+
+//Grab the argument
+var commands = process.argv[2];
+//Join the remaining arguments 
+var results = process.argv.slice(3).join(" ");
+
+//Liri.js can take in one of the following commands:
+switch (commands) {
+    case "concert-this":
+        concertThis (results);
+        break;
+
+    case "spotify-this-song":
+        spotifyThisSong (results);
+        break;
+
+    case "movie-this":
+        movieThis (results);
+        break;
+
+    case "do-what-it-says":
+        doWhatItSays (results);
+        break;
+}
+
+function concertThis (){
+    
+}
